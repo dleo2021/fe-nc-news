125367d5d6f0e9cbd67a91e6b18fde58dd687ade
dleo2021 <danleonard@hotmail.co.uk>
2024-08-05 15:11:52 +0100

    Small changes made based on feedback

---
+++ src/api.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://be-nc-news-o5xh.onrender.com/api",
+});
+
+export const getArticles = () => {
+  return api.get("/articles").then(({ data }) => {
+    console.log(articles);
+    return data.articles;
+  });
+};
+
+getArticles();
